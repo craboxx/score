1026b80d4786b0d820168ca0ea3c386c6483dcfd
craboxx <crabox.hash@gmail.com>
2025-12-10 10:29:08 +0530

    Update NupuramOverlay.jsx

--- src/components/NupuramOverlay.jsx
+++ src/components/NupuramOverlay.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 const NupuramOverlay = () => {
   return (
     <div className="nupuram-overlay">
-      <img src="/images/ad.png" alt="Nupuram" className="nupuram-image" />
+  <img 
+  src="https://craboxx.github.io/score/images/ad.png" 
+  alt="Nupuram" 
+  className="nupuram-image" 
+/>
     </div>
   );
 };
